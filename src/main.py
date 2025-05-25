b5005b0b12dc3dd76caab6bab928a62e63b5c43b
Santiago <santiago@bluefishai.com>
2025-05-25 20:22:56 +0200

    migrate docker image to uv

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from dotenv import load_dotenv
+
 import logging
 from fastapi import FastAPI
 
@@ -9,13 +11,15 @@ from api.register_routes import register_routes
 from api.rate_limiter import limiter, rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 
+load_dotenv()
+
 configure_logging(logging.DEBUG)
 
 app = FastAPI()
 
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, rate_limit_exceeded_handler)
-    
+
 app.add_middleware(RequestIdMiddleware)
 app.add_middleware(LoggerContextMiddleware)
 
